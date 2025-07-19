c447bfbb2509bc2a32e4bd35b2571836a1c1d2c1
duckyduckG <duckyduckg65@gmail.com>
2025-07-19 23:20:33 +0500

    drivers: pinctrl: fix misleading indentation

--- drivers/pinctrl/qcom/pinctrl-msm.c
+++ drivers/pinctrl/qcom/pinctrl-msm.c
@@ -582,7 +582,7 @@ static void msm_gpio_dbg_show(struct seq_file *s, struct gpio_chip *chip)
 		if (i < 4 || (i > 125 && i < 130))
 			continue;
 
-		msm_gpio_dbg_show_one(s, NULL, chip, i, gpio);
+	msm_gpio_dbg_show_one(s, NULL, chip, i, gpio);
 }
 
 #else
