c85e03e4151436d41a487116c0a2380d72f8b412
Xiaoyu Ye <benyxy@codeaurora.org>
2025-07-19 14:51:42 +0500

    ASoC: sm8150: enable widget WDMA3_OUT to ignore suspend
    Widget WDMA3_OUT is used for WDSP SVA 2ch. Enable this
    widget to ignore suspend to avoid WDSP SVA 2ch audio
    routing pathes being shutdown in suspend mode.
    
    Change-Id: I9a34ef3614eeb99d356192c70f1e1c79aaaced3c
    Signed-off-by: Xiaoyu Ye <benyxy@codeaurora.org>

--- techpack/audio/asoc/sm8150.c
+++ techpack/audio/asoc/sm8150.c
@@ -4316,7 +4316,8 @@ static int msm_audrx_init(struct snd_soc_pcm_runtime *rtd)
 	snd_soc_dapm_ignore_suspend(dapm, "SPK1 OUT");
 	snd_soc_dapm_ignore_suspend(dapm, "SPK2 OUT");
 	snd_soc_dapm_ignore_suspend(dapm, "AIF4 VI");
-	snd_soc_dapm_ignore_suspend(dapm, "VIINPUT");
+	snd_soc_dapm_ignore_suspend(dapm, "VIINPUT");	
+	snd_soc_dapm_ignore_suspend(dapm, "WDMA3_OUT");
 
 	if (!strcmp(dev_name(codec_dai->dev), "tavil_codec")) {
 #ifdef CONFIG_MACH_XIAOMI_SM8150
