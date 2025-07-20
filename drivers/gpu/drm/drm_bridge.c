e9c1d05351798d90aa70fb84ac998392c10ae971
duckyduckG <duckyduckg65@gmail.com>
2025-07-20 15:01:22 +0500

    drivers: gpu: Import xiaomi changes
    * vayu-r-oss

--- drivers/gpu/drm/drm_bridge.c
+++ drivers/gpu/drm/drm_bridge.c
@@ -270,8 +270,18 @@ void drm_bridge_post_disable(struct drm_bridge *bridge)
 	if (!bridge)
 		return;
 
+#if defined(CONFIG_MACH_XIAOMI_SM8150)
+	if (bridge->is_dsi_drm_bridge)
+		mutex_lock(&bridge->lock);
+#endif
+
 	if (bridge->funcs->post_disable)
 		bridge->funcs->post_disable(bridge);
+
+#if defined(CONFIG_MACH_XIAOMI_SM8150)
+	if (bridge->is_dsi_drm_bridge)
+		mutex_unlock(&bridge->lock);
+#endif
 
 	drm_bridge_post_disable(bridge->next);
 }
@@ -321,10 +331,46 @@ void drm_bridge_pre_enable(struct drm_bridge *bridge)
 
 	drm_bridge_pre_enable(bridge->next);
 
+#if defined(CONFIG_MACH_XIAOMI_SM8150)
+	if (bridge->is_dsi_drm_bridge)
+		mutex_lock(&bridge->lock);
+#endif
+
 	if (bridge->funcs->pre_enable)
 		bridge->funcs->pre_enable(bridge);
+
+#if defined(CONFIG_MACH_XIAOMI_SM8150)
+	if (bridge->is_dsi_drm_bridge)
+		mutex_unlock(&bridge->lock);
+#endif
 }
 EXPORT_SYMBOL(drm_bridge_pre_enable);
+
+#if defined(CONFIG_MACH_XIAOMI_SM8150)
+void drm_bridge_disp_param_set(struct drm_bridge *bridge, int cmd)
+{
+	if (!bridge)
+		return;
+
+	drm_bridge_disp_param_set(bridge->next, cmd);
+
+	if (bridge->funcs->disp_param_set)
+		bridge->funcs->disp_param_set(bridge, cmd);
+}
+
+int drm_get_panel_info(struct drm_bridge *bridge, char *buf)
+{
+	int rc = 0;
+	if (!bridge)
+		return rc;
+
+	if (bridge->funcs->disp_get_panel_info)
+		return bridge->funcs->disp_get_panel_info(bridge, buf);
+
+	return rc;
+}
+EXPORT_SYMBOL(drm_get_panel_info);
+#endif
 
 /**
  * drm_bridge_enable - enables all bridges in the encoder chain
