6c7051176ccc148179585bd793219273e9291a51
duckyduckG <duckyduckg65@gmail.com>
2025-07-26 21:48:00 +0500

    Revert "drivers: thermal: remove drm_notifier"
    This reverts commit abfe99c5cf65751301dc0c7df0f59605d740e041.

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -24,6 +24,10 @@
 #include <linux/suspend.h>
 #include <linux/cpu_cooling.h>
 
+#ifdef CONFIG_DRM
+#include <drm/drm_notifier_mi.h>
+#endif
+
 #define CREATE_TRACE_POINTS
 #include <trace/events/thermal.h>
 
@@ -54,6 +58,15 @@ static bool power_off_triggered;
 static struct thermal_governor *def_governor;
 
 static struct workqueue_struct *thermal_passive_wq;
+
+#ifdef CONFIG_DRM
+struct screen_monitor {
+	struct notifier_block thermal_notifier;
+	int screen_state; /* 1: on; 0:off */
+};
+
+struct screen_monitor sm;
+#endif
 
 static struct device thermal_message_dev;
 static atomic_t switch_mode = ATOMIC_INIT(-1);
@@ -1693,6 +1706,18 @@ static int of_parse_thermal_message(void)
 	return 0;
 }
 
+#ifdef CONFIG_DRM
+static ssize_t
+thermal_screen_state_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	return snprintf(buf, PAGE_SIZE, "%d\n", sm.screen_state);
+}
+
+static DEVICE_ATTR(screen_state, 0644,
+		thermal_screen_state_show, NULL);
+#endif
+
 static ssize_t
 thermal_sconfig_show(struct device *dev,
 				      struct device_attribute *attr, char *buf)
@@ -1858,6 +1883,11 @@ static int create_thermal_message_node(void)
 	dev_set_name(&thermal_message_dev, "thermal_message");
 	ret = device_register(&thermal_message_dev);
 	if (!ret) {
+#ifdef CONFIG_DRM
+		ret = sysfs_create_file(&thermal_message_dev.kobj, &dev_attr_screen_state.attr);
+		if (ret < 0)
+			pr_warn("Thermal: create batt message node failed\n");
+#endif
 		ret = sysfs_create_file(&thermal_message_dev.kobj, &dev_attr_sconfig.attr);
 		if (ret < 0)
 			pr_warn("Thermal: create sconfig node failed\n");
@@ -1903,8 +1933,59 @@ static void destroy_thermal_message_node(void)
 	sysfs_remove_file(&thermal_message_dev.kobj, &dev_attr_sconfig.attr);
 	sysfs_remove_file(&thermal_message_dev.kobj, &dev_attr_ambient_sensor_temp.attr);
 	sysfs_remove_file(&thermal_message_dev.kobj, &dev_attr_ambient_sensor.attr);
+#ifdef CONFIG_DRM
+	sysfs_remove_file(&thermal_message_dev.kobj, &dev_attr_screen_state.attr);
+#endif
 	device_unregister(&thermal_message_dev);
 }
+
+#ifdef CONFIG_DRM
+static const char *get_screen_state_name(int mode)
+{
+	switch (mode) {
+	case MI_DRM_BLANK_UNBLANK:
+		return "On";
+	case MI_DRM_BLANK_LP1:
+		return "Doze";
+	case MI_DRM_BLANK_LP2:
+		return "DozeSuspend";
+	case MI_DRM_BLANK_POWERDOWN:
+		return "Off";
+	default:
+		return "Unknown";
+    }
+}
+
+static int screen_state_for_thermal_callback(struct notifier_block *nb,
+		unsigned long val, void *data)
+{
+	struct mi_drm_notifier *evdata = data;
+	unsigned int blank;
+
+	if (val != MI_DRM_EVENT_BLANK || !evdata || !evdata->data)
+		return 0;
+
+	blank = *(int *)(evdata->data);
+	switch (blank) {
+	case MI_DRM_BLANK_UNBLANK:
+		sm.screen_state = 1;
+		break;
+	case MI_DRM_BLANK_LP1:
+	case MI_DRM_BLANK_LP2:
+	case MI_DRM_BLANK_POWERDOWN:
+		sm.screen_state = 0;
+		break;
+	default:
+		break;
+	}
+
+	pr_warn("%s: %s, sm.screen_state = %d\n", __func__, get_screen_state_name(blank),
+			sm.screen_state);
+	sysfs_notify(&thermal_message_dev.kobj, NULL, "screen_state");
+
+	return NOTIFY_OK;
+}
+#endif
 
 static int __init thermal_init(void)
 {
@@ -1947,6 +2028,13 @@ static int __init thermal_init(void)
 		pr_warn("Thermal: create thermal message node failed, return %d\n",
 			result);
 
+#ifdef CONFIG_DRM
+	sm.thermal_notifier.notifier_call = screen_state_for_thermal_callback;
+	if (mi_drm_register_client(&sm.thermal_notifier) < 0) {
+		pr_warn("Thermal: register screen state callback failed\n");
+	}
+#endif
+
 	return 0;
 
 exit_zone_parse:
@@ -1966,6 +2054,9 @@ error:
 
 static void thermal_exit(void)
 {
+#ifdef CONFIG_DRM
+	mi_drm_unregister_client(&sm.thermal_notifier);
+#endif
 	unregister_pm_notifier(&thermal_pm_nb);
 	of_thermal_destroy_zones();
 	destroy_workqueue(thermal_passive_wq);
