587ca5f7069536b7f6dd10f636e668d3a8c23e87
duckyduckG <duckyduckg65@gmail.com>
2025-08-11 23:13:05 +0500

    techpack: audio: mius: update for kernel 4.19

--- techpack/audio/asoc/msm-pcm-routing-v2.c
+++ techpack/audio/asoc/msm-pcm-routing-v2.c
@@ -31782,7 +31782,7 @@ static int msm_routing_probe(struct snd_soc_component *component)
 #endif
 	/* for mius start */
 #ifdef CONFIG_US_PROXIMITY
-	mius_add_platform_controls(platform);
+	mius_add_component_controls(component);
 #endif
 	/* for mius end */
 
