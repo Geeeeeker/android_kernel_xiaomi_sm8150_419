6f08ee0baf4871e7318f54a39ed583382f8ab542
duckyduckG <duckyduckg65@gmail.com>
2025-09-13 14:07:36 +0500

    video: vidc: bring back crop_info and buf_req
    * partial fix for decoding

--- techpack/video/msm/vidc/hfi_response_handler.c
+++ techpack/video/msm/vidc/hfi_response_handler.c
@@ -109,6 +109,8 @@ static int hfi_process_sess_evt_seq_changed(u32 device_id,
 	struct hfi_profile_level *profile_level;
 	struct hfi_bit_depth *pixel_depth;
 	struct hfi_pic_struct *pic_struct;
+	struct hfi_buffer_requirements *buf_req;
+	struct hfi_index_extradata_input_crop_payload *crop_info;
 	struct hfi_dpb_counts *dpb_counts;
 	u32 rem_size,entropy_mode = 0;
 	u8 *data_ptr;
@@ -262,6 +264,13 @@ static int hfi_process_sess_evt_seq_changed(u32 device_id,
 					hfi_buffer_requirements)))
 					return -E2BIG;
 				data_ptr = data_ptr + sizeof(u32);
+				buf_req =
+					(struct hfi_buffer_requirements *)
+						data_ptr;
+				event_notify.fw_min_cnt =
+					buf_req->buffer_count_min;
+				s_vpr_hp(sid, "Capture Count : 0x%x\n",
+						event_notify.fw_min_cnt);
 				data_ptr +=
 					sizeof(struct hfi_buffer_requirements);
 				rem_size -=
@@ -271,7 +280,23 @@ static int hfi_process_sess_evt_seq_changed(u32 device_id,
 				if (!validate_pkt_size(rem_size, sizeof(struct
 				     hfi_index_extradata_input_crop_payload)))
 					return -E2BIG;
-				data_ptr = data_ptr + sizeof(u32);
+				data_ptr = data_ptr + sizeof(u32); // SM8150
+				crop_info = (struct
+				hfi_index_extradata_input_crop_payload *)
+						data_ptr;
+				event_notify.crop_data.left = crop_info->left;
+				event_notify.crop_data.top = crop_info->top;
+				event_notify.crop_data.width = crop_info->width;
+				event_notify.crop_data.height =
+					crop_info->height;
+				s_vpr_h(sid,
+					"CROP info : Left = %d Top = %d\n",
+						crop_info->left,
+						crop_info->top);
+				s_vpr_h(sid,
+					"CROP info : Width = %d Height = %d\n",
+						crop_info->width,
+						crop_info->height); // SM8150
 				data_ptr +=
 					sizeof(struct
 					hfi_index_extradata_input_crop_payload);
