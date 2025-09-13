44db923ba00114659cdc8b2ffeb7e3eee2e9b28f
duckyduckG <duckyduckg65@gmail.com>
2025-09-13 15:15:24 +0500

    techpack: camera: import minimal xiaomi changes

--- techpack/camera/include/uapi/media/cam_defs.h
+++ techpack/camera/include/uapi/media/cam_defs.h
@@ -10,6 +10,11 @@
 #include <linux/types.h>
 #include <linux/ioctl.h>
 
+ 
+#if 1
+#define CAM_UPDATE_REG                          (CAM_EXT_OPCODE_BASE + 0x2)
+#define CAM_READ_REG                            (CAM_EXT_OPCODE_BASE + 0x3)
+#endif
 
 /* camera op codes */
 #define CAM_COMMON_OPCODE_BASE                  0x100
