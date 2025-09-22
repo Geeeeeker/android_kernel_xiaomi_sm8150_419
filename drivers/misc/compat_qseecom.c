561d722a952d63eccb9c56c3e259a1211a20fbb9
Jiten Patel <jitepate@codeaurora.org>
2025-09-22 23:43:02 +0500

    compat_qseecom: Add new IOCTL for new FDE design Add new IOCTL COMPAT_QSEECOM_IOCTL_SET_ICE_INFO allow 32-bit userland programs to make ioctl calls on a 64-bit kernel needed as per new FDE design.
    Change-Id: I740eae9d33fc58fe9cedd41c3275c0d366e36fb4
    Signed-off-by: Jiten Patel <jitepate@codeaurora.org>

--- drivers/misc/compat_qseecom.c
+++ drivers/misc/compat_qseecom.c
@@ -467,6 +467,18 @@ static int compat_put_qseecom_is_es_activated_req(
 	return err;
 }
 
+static int compat_get_qseecom_ice_flag(
+	struct compat_qseecom_ice_data_t __user *data32,
+	struct qseecom_ice_data_t __user *data)
+{
+	int err;
+	compat_int_t ice_flag;
+
+	err = get_user(ice_flag, &data32->flag);
+	err |= put_user(ice_flag, &data->flag);
+	return err;
+}
+
 static unsigned int convert_cmd(unsigned int cmd)
 {
 	switch (cmd) {
@@ -530,7 +542,8 @@ static unsigned int convert_cmd(unsigned int cmd)
 		return QSEECOM_IOCTL_SEND_MODFD_CMD_64_REQ;
 	case COMPAT_QSEECOM_IOCTL_SEND_MODFD_RESP_64:
 		return QSEECOM_IOCTL_SEND_MODFD_RESP_64;
-
+	case COMPAT_QSEECOM_IOCTL_SET_ICE_INFO:
+		return QSEECOM_IOCTL_SET_ICE_INFO;
 	default:
 		return cmd;
 	}
@@ -904,6 +917,24 @@ long compat_qseecom_ioctl(struct file *file,
 
 		return qseecom_ioctl(file, convert_cmd(cmd),
 						(unsigned long)data);
+	}
+	break;
+	case COMPAT_QSEECOM_IOCTL_SET_ICE_INFO: {
+		struct compat_qseecom_ice_data_t __user *data32;
+		struct qseecom_ice_data_t __user *data;
+		int err;
+
+		data32 = compat_ptr(arg);
+		data = compat_alloc_user_space(sizeof(*data));
+		if (data == NULL)
+			return -EFAULT;
+
+		err = compat_get_qseecom_ice_flag(data32, data);
+		if (err)
+			return err;
+
+		return qseecom_ioctl(file, convert_cmd(cmd),
+				(unsigned long)data);
 	}
 	break;
 	default:
