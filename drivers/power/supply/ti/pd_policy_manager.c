df6ae938630e0ba9697af77a354be96be77368e5
duckyduckG <duckyduckg65@gmail.com>
2025-10-03 16:21:20 +0500

    power:supply: ti: add correct prefix

--- drivers/power/supply/ti/pd_policy_manager.c
+++ drivers/power/supply/ti/pd_policy_manager.c
@@ -1378,7 +1378,7 @@ static void usb_psy_change_work(struct work_struct *work)
 	ret = power_supply_get_property(
 		pdpm->usb_psy, POWER_SUPPLY_PROP_TYPEC_POWER_ROLE, &val);
 	if (ret) {
-		pr_err("Failed to read typec power role\n");
+		pr_err("PR: Failed to read typec power role\n");
 		goto out;
 	}
 
@@ -1397,7 +1397,7 @@ static void usb_psy_change_work(struct work_struct *work)
 					POWER_SUPPLY_PROP_PD_AUTHENTICATION,
 					&pd_auth_val);
 	if (ret) {
-		pr_err("Failed to read typec power role\n");
+		pr_err("AUTHEN: Failed to read typec power role\n");
 		goto out;
 	}
 
