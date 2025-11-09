7cf97556aa27786e9a03e1894836c3a41dfe1637
duckyduckG <duckyduckg65@gmail.com>
2025-11-09 23:42:07 +0500

    input: fp: fpc: DEBUG

--- drivers/input/fingerprint/fpc_tee/fpc1020_tee.c
+++ drivers/input/fingerprint/fpc_tee/fpc1020_tee.c
@@ -24,6 +24,9 @@
  * modify it under the terms of the GNU General Public License Version 2
  * as published by the Free Software Foundation.
  */
+
+#define DEBUG
+
 #include <linux/atomic.h>
 #include <linux/delay.h>
 #include <linux/gpio.h>
