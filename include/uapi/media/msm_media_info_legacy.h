da658c7b9a7a7331ed712f30c48f9e5e993ce078
duckyduckG <duckyduckg65@gmail.com>
2026-01-31 02:33:13 +0500

    uapi: media: Add msm_media_info header for sdm845
    this is a copy of msm_media_info.h and is required for common kernel to prevent conflict with KONA,
    following commit is already reverted.
    4e5d64083e8f msm: media: uapi: Redefine NV12 format with different alignment
    
    Co-Authored-By: Edwin Moquete <edwinmmoquete@gmail.com>

--- include/uapi/media/msm_media_info_legacy.h
+++ include/uapi/media/msm_media_info_legacy.h
@@ -1,27 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
-#ifndef __MSM_MEDIA_INFO_H__
-#define __MSM_MEDIA_INFO_H__
-
-#ifdef CONFIG_ARCH_SDM845
-#include "msm_media_info_legacy.h"
-#else
-
-#include <asm/bitsperlong.h>
-
-#if __BITS_PER_LONG == 64
-#define NV12_STRIDE_ALIGNMENT 512
-#define NV12_SCANLINE_ALIGNMENT 512
-#else
-#define NV12_STRIDE_ALIGNMENT 128
-#define NV12_SCANLINE_ALIGNMENT 32
-#endif
-
-#ifdef VENUS_USE_64BIT_ALIGNMENT
-#undef NV12_STRIDE_ALIGNMENT
-#undef NV12_SCANLINE_ALIGNMENT
-#define NV12_STRIDE_ALIGNMENT 512
-#define NV12_SCANLINE_ALIGNMENT 512
-#endif
+#ifndef __MSM_MEDIA_INFO_LEGACY_H__
+#define __MSM_MEDIA_INFO_LEGACY_H__
 
 /* Width and Height should be multiple of 16 */
 #define INTERLACE_WIDTH_MAX 1920
@@ -66,10 +45,10 @@ enum color_fmts {
 	 * . . . . . . . . . . . . . . . .  V
 	 * . . . . . . . . . . . . . . . .  --> Buffer size alignment
 	 *
-	 * Y_Stride : Width aligned to 512 or 128
-	 * UV_Stride : Width aligned to 512 or 128
-	 * Y_Scanlines: Height aligned to 512 or 32
-	 * UV_Scanlines: Height/2 aligned to 256 or 16
+	 * Y_Stride : Width aligned to 512
+	 * UV_Stride : Width aligned to 512
+	 * Y_Scanlines: Height aligned to 512
+	 * UV_Scanlines: Height/2 aligned to 256
 	 * Total size = align(Y_Stride * Y_Scanlines
 	 *          + UV_Stride * UV_Scanlines, 4096)
 	 */
@@ -136,10 +115,10 @@ enum color_fmts {
 	 * . . . . . . . . . . . . . . . .  V
 	 * . . . . . . . . . . . . . . . .  --> Padding & Buffer size alignment
 	 *
-	 * Y_Stride : Width aligned to 512 or 128
-	 * UV_Stride : Width aligned to 512 or 128
-	 * Y_Scanlines: Height aligned to 512 or 32
-	 * UV_Scanlines: Height/2 aligned to 256 or 16
+	 * Y_Stride : Width aligned to 512
+	 * UV_Stride : Width aligned to 512
+	 * Y_Scanlines: Height aligned to 512
+	 * UV_Scanlines: Height/2 aligned to 256
 	 * Total size = align(Y_Stride * Y_Scanlines
 	 *          + UV_Stride * UV_Scanlines, 4096)
 	 */
@@ -820,6 +799,18 @@ enum color_fmts {
 	COLOR_FMT_NV12_512,
 };
 
+static inline unsigned int VENUS_EXTRADATA_SIZE(int width, int height)
+{
+	(void)height;
+	(void)width;
+
+	/*
+	 * In the future, calculate the size based on the w/h but just
+	 * hardcode it for now since 16K satisfies all current usecases.
+	 */
+	return 16 * 1024;
+}
+
 /*
  * Function arguments:
  * @color_fmt
@@ -838,9 +829,6 @@ static inline unsigned int VENUS_Y_STRIDE(unsigned int color_fmt,
 	switch (color_fmt) {
 	case COLOR_FMT_NV12:
 	case COLOR_FMT_NV21:
-		alignment = NV12_STRIDE_ALIGNMENT;
-		stride = MSM_MEDIA_ALIGN(width, alignment);
-		break;
 	case COLOR_FMT_NV12_512:
 		alignment = 512;
 		stride = MSM_MEDIA_ALIGN(width, alignment);
@@ -885,9 +873,6 @@ static inline unsigned int VENUS_UV_STRIDE(unsigned int color_fmt,
 	switch (color_fmt) {
 	case COLOR_FMT_NV21:
 	case COLOR_FMT_NV12:
-		alignment = NV12_STRIDE_ALIGNMENT;
-		stride = MSM_MEDIA_ALIGN(width, alignment);
-		break;
 	case COLOR_FMT_NV12_512:
 		alignment = 512;
 		stride = MSM_MEDIA_ALIGN(width, alignment);
@@ -932,8 +917,6 @@ static inline unsigned int VENUS_Y_SCANLINES(unsigned int color_fmt,
 	switch (color_fmt) {
 	case COLOR_FMT_NV12:
 	case COLOR_FMT_NV21:
-		alignment = NV12_SCANLINE_ALIGNMENT;
-		break;
 	case COLOR_FMT_NV12_512:
 		alignment = 512;
 		break;
@@ -972,8 +955,6 @@ static inline unsigned int VENUS_UV_SCANLINES(unsigned int color_fmt,
 	switch (color_fmt) {
 	case COLOR_FMT_NV21:
 	case COLOR_FMT_NV12:
-		alignment = NV12_SCANLINE_ALIGNMENT/2;
-		break;
 	case COLOR_FMT_NV12_512:
 		alignment = 256;
 		break;
@@ -1419,5 +1400,4 @@ invalid_input:
 	return size;
 }
 
-#endif /* CONFIG_ARCH_SDM845 */
-#endif
+#endif /* __MSM_MEDIA_INFO_LEGACY_H__ */
