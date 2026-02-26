duckyduckG/android_kernel_xiaomi_sm8150_414

techpack: audio: Import tas256x and msm-cirrus

techpack/audio/include/dsp/msm-cirrus-playback.h
@@ -1,0 +1,183 @@
+/* Copyright (c) 2015 Cirrus Logic, Inc.
+ * Copyright (C) 2019 XiaoMi, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#ifndef MSM_CIRRUS_PLAYBACK_H
+#define MSM_CIRRUS_PLAYBACK_H
+#define DEBUG
+#include <linux/slab.h>
+#include <sound/soc.h>
+#include <dsp/apr_audio-v2.h>
+#include <dsp/q6afe-v2.h>
+#include <dsp/q6audio-v2.h>
+#include <uapi/sound/msm-cirrus-playback.h>
+
+#define MIN_CHAN_SWAP_SAMPLES 48
+#define MAX_CHAN_SWAP_SAMPLES 9600
+#define VOL_ATTN_MAX 0x7FFFFFFF
+#define VOL_ATTN_18DB 257698038
+#define VOL_ATTN_24DB 128849019
+
+struct afe_cspl_state {
+	void **apr;
+	atomic_t *status;
+	atomic_t *state;
+	wait_queue_head_t *wait;
+	int timeout_ms;
+};
+
+extern struct afe_cspl_state cspl_afe;
+
+struct afe_custom_crus_set_config_v2_t {
+	struct apr_hdr hdr;
+	struct afe_port_cmd_set_param_v2 param;
+	struct param_hdr_v2 data;
+} __packed;
+
+struct afe_custom_crus_get_config_v2_t {
+	struct apr_hdr hdr;
+	struct afe_port_cmd_get_param_v2 param;
+	struct param_hdr_v2 data;
+} __packed;
+
+struct afe_custom_crus_set_config_t {
+	struct apr_hdr hdr;
+	struct afe_port_cmd_set_param_v3 param;
+	struct param_hdr_v3 data;
+} __packed;
+
+struct afe_custom_crus_get_config_t {
+	struct apr_hdr hdr;
+	struct afe_port_cmd_get_param_v3 param;
+	struct param_hdr_v3 data;
+} __packed;
+
+/* Payload struct for getting or setting one integer value from/to the DSP module */
+struct crus_single_data_t {
+	int32_t value;
+};
+
+/* Payload struct for getting or setting two integer values from/to the DSP module */
+struct crus_dual_data_t {
+	int32_t data1;
+	int32_t data2;
+};
+
+/* Payload struct for getting or setting three integer values from/to the DSP module */
+struct crus_triple_data_t {
+	int32_t data1;
+	int32_t data2;
+	int32_t data3;
+};
+
+/* Payload struct for setting the RX and TX use cases */
+struct crus_rx_run_case_ctrl_t {
+	int32_t value;
+	int32_t status_l;
+	int32_t checksum_l;
+	int32_t z_l;
+	int32_t status_r;
+	int32_t checksum_r;
+	int32_t z_r;
+	int32_t atemp;
+};
+
+/* Payload struct for getting calibration result from DSP module */
+struct cirrus_cal_result_t {
+	int32_t status_l;
+	int32_t checksum_l;
+	int32_t z_l;
+	int32_t status_r;
+	int32_t checksum_r;
+	int32_t z_r;
+};
+
+#define APR_CHUNK_SIZE 256
+
+/* Payload struct for sending an external configuration string to the DSP
+ * module
+ */
+struct crus_external_config_t {
+	uint32_t total_size;
+	uint32_t chunk_size;
+	int32_t done;
+	int32_t reserved;
+	int32_t config;
+	char data[APR_CHUNK_SIZE];
+};
+/* Payload struct for sending an external tuning transition string to the DSP
+ * module
+ */
+struct crus_delta_config_t {
+	uint32_t total_size;
+	uint32_t chunk_size;
+	int32_t done;
+	int32_t index;
+	int32_t reserved;
+	int32_t config;
+	char data[APR_CHUNK_SIZE];
+};
+
+struct crus_rx_temperature_t {
+	uint32_t cal_status_l;
+	uint32_t temp_r;
+	uint32_t z_r;
+	uint32_t temp_l;
+	uint32_t z_l;
+	uint32_t cal_status_r;
+	uint32_t amb_temp_l;
+	uint32_t excur_model_r;
+	uint32_t excur_model_l;
+	uint32_t cksum_r;
+	uint32_t amb_temp_r;
+	uint32_t cksum_l;
+	uint32_t hp_status_l;
+	uint32_t full_status_l;
+	uint32_t hp_status_r;
+	uint32_t full_status_r;
+};
+
+#define CONFIG_FILE_SIZE 128
+#define PAYLOAD_FOLLOWS_CONFIG 4
+#define MAX_TUNING_CONFIGS 4
+#define MIN_CHAN_SWAP_SAMPLES 48
+#define MAX_CHAN_SWAP_SAMPLES 9600
+#define CRUS_MAX_BUFFER_SIZE 384
+
+struct crus_control_t {
+	struct device *device;
+	int32_t q6afe_rev;
+	bool afe_start;
+	bool enable;
+	bool prot_en;
+	int32_t fb_port_index;
+	int32_t fb_port;
+	int32_t ff_port;
+	int ch_sw_duration;
+	int32_t ch_sw;
+	int32_t vol_atten;
+	atomic_t callback_wait;
+	atomic_t count_wait;
+	struct mutex param_lock;
+	struct mutex sp_lock;
+	int32_t *user_buffer;
+	int32_t usecase;
+	int32_t config;
+	int32_t conf_sel;
+	int32_t delta_sel;
+	int32_t usecase_dt_count;
+};
+
+//extern int afe_apr_send_pkt_crus(void *data, int index, int set);
+
+int crus_afe_callback(void *payload, int size);
+void msm_crus_pb_add_controls(struct snd_soc_platform *platform);
+#endif /* _MSM_CIRRUS_PLAYBACK_H */

techpack/audio/include/dsp/tas_smart_amp_v2.h
@@ -1,0 +1,177 @@
+#ifndef __TAS_SMART_AMP__
+#define __TAS_SMART_AMP__
+
+/*Should be defined in configuration file*/
+//#define CONFIG_TAS25XX_ALGO
+#define CONFIG_TAS25XX_ALGO_STEREO
+#define CONFIG_SET_RE_IN_KERNEL
+
+//FIXME
+//update this value at OEM/ODM
+//#pragma message ("########### FIXME ################")
+#define SMARTAMP_SPEAKER_CALIBDATA_FILE                                        \
+	"/mnt/vendor/persist/audio/tas25xx_calib.bin"
+//#define SMARTAMP_SPEAKER_CALIBDATA_FILE "/persist/audio/smartamp_calib.bin"
+
+#define APR_VERSION_V2 0
+#define APR_VERSION_V3 1
+
+#include <linux/types.h>
+//#if APR_VERSION_V2
+#include <dsp/apr_audio-v2.h>
+#include <sound/soc.h>
+//#else
+//#endif
+#include <linux/delay.h>
+
+#define AFE_SMARTAMP_MODULE_RX 0x11111112
+#define AFE_SMARTAMP_MODULE_TX 0x11111111
+
+#define CAPI_V2_TAS_TX_ENABLE 0x10012D14
+#define CAPI_V2_TAS_TX_CFG 0x10012D16
+#define CAPI_V2_TAS_RX_ENABLE 0x10012D13
+#define CAPI_V2_TAS_RX_CFG 0x10012D15
+
+#define MAX_DSP_PARAM_INDEX 2048
+
+#define TAS_PAYLOAD_SIZE 14
+#define TAS_GET_PARAM 1
+#define TAS_SET_PARAM 0
+
+//FIXME
+//Update as needed
+#define TAS_RX_PORT AFE_PORT_ID_QUATERNARY_MI2S_RX
+#define TAS_TX_PORT AFE_PORT_ID_QUATERNARY_MI2S_TX
+
+#define CHANNEL0 1
+#define CHANNEL1 2
+
+#define TRUE 1
+#define FALSE 0
+
+#define TAS_SA_GET_F0 3810
+#define TAS_SA_GET_Q 3811
+#define TAS_SA_GET_TV 3812
+#define TAS_SA_GET_RE 3813
+#define TAS_SA_CALIB_INIT 3814
+#define TAS_SA_CALIB_DEINIT 3815
+#define TAS_SA_SET_RE 3816
+#define TAS_SA_F0_TEST_INIT 3817
+#define TAS_SA_F0_TEST_DEINIT 3818
+#define TAS_SA_SET_PROFILE 3819
+#define TAS_SA_GET_STATUS 3821
+#define TAS_SA_SET_SPKID 3822
+#define TAS_SA_SET_TCAL 3823
+
+#define TAS_SA_IV_VBAT_FMT 3825
+/*Added for DC Detection*/
+#define CAPI_V2_TAS_SA_DC_DETECT 0x40404040
+
+#define CALIB_START 1
+#define CALIB_STOP 2
+#define TEST_START 3
+#define TEST_STOP 4
+
+#define SLAVE1 0x98
+#define SLAVE2 0x9A
+#define SLAVE3 0x9C
+#define SLAVE4 0x9E
+
+#define CHANNEL0 1
+#define CHANNEL1 2
+
+#define TAS_SA_IS_SPL_IDX(X) ((((X) >= 3810) && ((X) < 3899)) ? 1 : 0)
+#define TAS_CALC_PARAM_IDX(I, LEN, CH) ((I) | (LEN << 16) | (CH << 24))
+#define AFE_SA_IS_SPL_IDX(X) ((((X) >= 3810) && ((X) < 3899)) ? 1 : 0)
+/* To Decode the Param ID */
+#define TAS_PARAM_IDX_GET_ID(X) (0xFFFF & (X))
+#define TAS_PARAM_IDX_GET_LEN(X) ((0x0FFF0000 & (X)) >> 16)
+#define TAS_PARAM_IDX_GET_CH(X) ((0xF0000000 & (X)) >> 24)
+
+typedef enum {
+	IV_SENSE_FORMAT_NO_VBAT = 0,
+	IV_SENSE_FORMAT_12_BIT_WITH_8BIT_VBAT = 1,
+	IV_SENSE_FORMAT_8_BIT_WITH_8BIT_VBAT = 2,
+} ti_smartamp_iv_vbat_format_t;
+
+/*
+ * List all the other profiles other than none and calibration.
+ */
+#define TAS_ALGO_PROFILE_LIST "MUSIC", "VOICE", "VOIP", "RINGTONE"
+
+struct afe_smartamp_get_set_params_t {
+	uint32_t payload[TAS_PAYLOAD_SIZE];
+} __packed;
+
+#if APR_VERSION_V2
+struct afe_smartamp_config_command {
+	struct apr_hdr hdr;
+	struct afe_port_cmd_set_param_v2 param;
+	struct afe_port_param_data_v2 pdata;
+	struct afe_smartamp_get_set_params_t prot_config;
+} __packed;
+
+struct afe_smartamp_get_calib {
+	struct apr_hdr hdr;
+	struct afe_port_cmd_get_param_v2 get_param;
+	struct afe_port_param_data_v2 pdata;
+	struct afe_smartamp_get_set_params_t res_cfg;
+} __packed;
+
+struct afe_smartamp_calib_get_resp {
+	uint32_t status;
+	struct afe_port_param_data_v2 pdata;
+	struct afe_smartamp_get_set_params_t res_cfg;
+} __packed;
+
+#elif APR_VERSION_V3
+
+//#define BUILD_STUB
+/* only for build test */
+#ifdef BUILD_STUB
+#define INSTANCE_ID_0 0
+#define AFE_PORT_CMDRSP_GET_PARAM_V3 0
+struct param_hdr_v3 {
+	int module_id;
+	int instance_id;
+	int param_id;
+	int param_size;
+};
+
+struct mem_mapping_hdr {};
+
+#endif
+
+struct afe_smartamp_get_calib {
+	struct apr_hdr hdr;
+	struct mem_mapping_hdr mem_hdr;
+	struct param_hdr_v3 pdata;
+	struct afe_smartamp_get_set_params_t res_cfg;
+} __packed;
+
+struct afe_smartamp_calib_get_resp {
+	uint32_t status;
+	struct param_hdr_v3 pdata;
+	struct afe_smartamp_get_set_params_t res_cfg;
+} __packed;
+#endif
+
+typedef struct {
+	int channel;
+} dc_detection_data_t;
+
+void register_tas256x_reset_func(void *fptr, dc_detection_data_t *data);
+int afe_tas_smartamp_get_calib_data(uint32_t module_id, uint32_t param_id,
+				    int32_t length, uint8_t *data);
+int afe_tas_smartamp_set_calib_data(uint32_t module_id, uint32_t param_id,
+				    int32_t length, uint8_t *data);
+
+int tas25xx_smartamp_algo_ctrl(u8 *user_data, uint32_t param_id,
+			       uint8_t get_set, uint32_t length,
+			       uint32_t module_id);
+
+void tas_smartamp_add_algo_controls(struct snd_soc_codec *codec);
+//void tas_smartamp_add_algo_controls_for_platform (struct snd_soc_platform *platform);
+void tas_smartamp_remove_algo_controls(struct snd_soc_codec *codec);
+bool tas25xx_set_iv_bit_fomat(int iv_data_with, int vbat, int update_now);
+#endif

Geeeeeker/android_kernel_xiaomi_sm8150_419

techpack/audio/include/dsp/msm-cirrus-playback.h
@@ -1,0 +1,183 @@
+/* Copyright (c) 2015 Cirrus Logic, Inc.
+ * Copyright (C) 2019 XiaoMi, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#ifndef MSM_CIRRUS_PLAYBACK_H
+#define MSM_CIRRUS_PLAYBACK_H
+#define DEBUG
+#include <linux/slab.h>
+#include <sound/soc.h>
+#include <dsp/apr_audio-v2.h>
+#include <dsp/q6afe-v2.h>
+#include <dsp/q6audio-v2.h>
+#include <uapi/sound/msm-cirrus-playback.h>
+
+#define MIN_CHAN_SWAP_SAMPLES 48
+#define MAX_CHAN_SWAP_SAMPLES 9600
+#define VOL_ATTN_MAX 0x7FFFFFFF
+#define VOL_ATTN_18DB 257698038
+#define VOL_ATTN_24DB 128849019
+
+struct afe_cspl_state {
+	void **apr;
+	atomic_t *status;
+	atomic_t *state;
+	wait_queue_head_t *wait;
+	int timeout_ms;
+};
+
+extern struct afe_cspl_state cspl_afe;
+
+struct afe_custom_crus_set_config_v2_t {
+	struct apr_hdr hdr;
+	struct afe_port_cmd_set_param_v2 param;
+	struct param_hdr_v2 data;
+} __packed;
+
+struct afe_custom_crus_get_config_v2_t {
+	struct apr_hdr hdr;
+	struct afe_port_cmd_get_param_v2 param;
+	struct param_hdr_v2 data;
+} __packed;
+
+struct afe_custom_crus_set_config_t {
+	struct apr_hdr hdr;
+	struct afe_port_cmd_set_param_v3 param;
+	struct param_hdr_v3 data;
+} __packed;
+
+struct afe_custom_crus_get_config_t {
+	struct apr_hdr hdr;
+	struct afe_port_cmd_get_param_v3 param;
+	struct param_hdr_v3 data;
+} __packed;
+
+/* Payload struct for getting or setting one integer value from/to the DSP module */
+struct crus_single_data_t {
+	int32_t value;
+};
+
+/* Payload struct for getting or setting two integer values from/to the DSP module */
+struct crus_dual_data_t {
+	int32_t data1;
+	int32_t data2;
+};
+
+/* Payload struct for getting or setting three integer values from/to the DSP module */
+struct crus_triple_data_t {
+	int32_t data1;
+	int32_t data2;
+	int32_t data3;
+};
+
+/* Payload struct for setting the RX and TX use cases */
+struct crus_rx_run_case_ctrl_t {
+	int32_t value;
+	int32_t status_l;
+	int32_t checksum_l;
+	int32_t z_l;
+	int32_t status_r;
+	int32_t checksum_r;
+	int32_t z_r;
+	int32_t atemp;
+};
+
+/* Payload struct for getting calibration result from DSP module */
+struct cirrus_cal_result_t {
+	int32_t status_l;
+	int32_t checksum_l;
+	int32_t z_l;
+	int32_t status_r;
+	int32_t checksum_r;
+	int32_t z_r;
+};
+
+#define APR_CHUNK_SIZE 256
+
+/* Payload struct for sending an external configuration string to the DSP
+ * module
+ */
+struct crus_external_config_t {
+	uint32_t total_size;
+	uint32_t chunk_size;
+	int32_t done;
+	int32_t reserved;
+	int32_t config;
+	char data[APR_CHUNK_SIZE];
+};
+/* Payload struct for sending an external tuning transition string to the DSP
+ * module
+ */
+struct crus_delta_config_t {
+	uint32_t total_size;
+	uint32_t chunk_size;
+	int32_t done;
+	int32_t index;
+	int32_t reserved;
+	int32_t config;
+	char data[APR_CHUNK_SIZE];
+};
+
+struct crus_rx_temperature_t {
+	uint32_t cal_status_l;
+	uint32_t temp_r;
+	uint32_t z_r;
+	uint32_t temp_l;
+	uint32_t z_l;
+	uint32_t cal_status_r;
+	uint32_t amb_temp_l;
+	uint32_t excur_model_r;
+	uint32_t excur_model_l;
+	uint32_t cksum_r;
+	uint32_t amb_temp_r;
+	uint32_t cksum_l;
+	uint32_t hp_status_l;
+	uint32_t full_status_l;
+	uint32_t hp_status_r;
+	uint32_t full_status_r;
+};
+
+#define CONFIG_FILE_SIZE 128
+#define PAYLOAD_FOLLOWS_CONFIG 4
+#define MAX_TUNING_CONFIGS 4
+#define MIN_CHAN_SWAP_SAMPLES 48
+#define MAX_CHAN_SWAP_SAMPLES 9600
+#define CRUS_MAX_BUFFER_SIZE 384
+
+struct crus_control_t {
+	struct device *device;
+	int32_t q6afe_rev;
+	bool afe_start;
+	bool enable;
+	bool prot_en;
+	int32_t fb_port_index;
+	int32_t fb_port;
+	int32_t ff_port;
+	int ch_sw_duration;
+	int32_t ch_sw;
+	int32_t vol_atten;
+	atomic_t callback_wait;
+	atomic_t count_wait;
+	struct mutex param_lock;
+	struct mutex sp_lock;
+	int32_t *user_buffer;
+	int32_t usecase;
+	int32_t config;
+	int32_t conf_sel;
+	int32_t delta_sel;
+	int32_t usecase_dt_count;
+};
+
+//extern int afe_apr_send_pkt_crus(void *data, int index, int set);
+
+int crus_afe_callback(void *payload, int size);
+void msm_crus_pb_add_controls(struct snd_soc_component *component);
+#endif /* _MSM_CIRRUS_PLAYBACK_H */

techpack/audio/include/dsp/tas_smart_amp_v2.h
@@ -1,0 +1,177 @@
+#ifndef __TAS_SMART_AMP__
+#define __TAS_SMART_AMP__
+
+/*Should be defined in configuration file*/
+//#define CONFIG_TAS25XX_ALGO
+#define CONFIG_TAS25XX_ALGO_STEREO
+#define CONFIG_SET_RE_IN_KERNEL
+
+//FIXME
+//update this value at OEM/ODM
+//#pragma message ("########### FIXME ################")
+#define SMARTAMP_SPEAKER_CALIBDATA_FILE                                        \
+	"/mnt/vendor/persist/audio/tas25xx_calib.bin"
+//#define SMARTAMP_SPEAKER_CALIBDATA_FILE "/persist/audio/smartamp_calib.bin"
+
+#define APR_VERSION_V2 0
+#define APR_VERSION_V3 1
+
+#include <linux/types.h>
+//#if APR_VERSION_V2
+#include <dsp/apr_audio-v2.h>
+#include <sound/soc.h>
+//#else
+//#endif
+#include <linux/delay.h>
+
+#define AFE_SMARTAMP_MODULE_RX 0x11111112
+#define AFE_SMARTAMP_MODULE_TX 0x11111111
+
+#define CAPI_V2_TAS_TX_ENABLE 0x10012D14
+#define CAPI_V2_TAS_TX_CFG 0x10012D16
+#define CAPI_V2_TAS_RX_ENABLE 0x10012D13
+#define CAPI_V2_TAS_RX_CFG 0x10012D15
+
+#define MAX_DSP_PARAM_INDEX 2048
+
+#define TAS_PAYLOAD_SIZE 14
+#define TAS_GET_PARAM 1
+#define TAS_SET_PARAM 0
+
+//FIXME
+//Update as needed
+#define TAS_RX_PORT AFE_PORT_ID_QUATERNARY_MI2S_RX
+#define TAS_TX_PORT AFE_PORT_ID_QUATERNARY_MI2S_TX
+
+#define CHANNEL0 1
+#define CHANNEL1 2
+
+#define TRUE 1
+#define FALSE 0
+
+#define TAS_SA_GET_F0 3810
+#define TAS_SA_GET_Q 3811
+#define TAS_SA_GET_TV 3812
+#define TAS_SA_GET_RE 3813
+#define TAS_SA_CALIB_INIT 3814
+#define TAS_SA_CALIB_DEINIT 3815
+#define TAS_SA_SET_RE 3816
+#define TAS_SA_F0_TEST_INIT 3817
+#define TAS_SA_F0_TEST_DEINIT 3818
+#define TAS_SA_SET_PROFILE 3819
+#define TAS_SA_GET_STATUS 3821
+#define TAS_SA_SET_SPKID 3822
+#define TAS_SA_SET_TCAL 3823
+
+#define TAS_SA_IV_VBAT_FMT 3825
+/*Added for DC Detection*/
+#define CAPI_V2_TAS_SA_DC_DETECT 0x40404040
+
+#define CALIB_START 1
+#define CALIB_STOP 2
+#define TEST_START 3
+#define TEST_STOP 4
+
+#define SLAVE1 0x98
+#define SLAVE2 0x9A
+#define SLAVE3 0x9C
+#define SLAVE4 0x9E
+
+#define CHANNEL0 1
+#define CHANNEL1 2
+
+#define TAS_SA_IS_SPL_IDX(X) ((((X) >= 3810) && ((X) < 3899)) ? 1 : 0)
+#define TAS_CALC_PARAM_IDX(I, LEN, CH) ((I) | (LEN << 16) | (CH << 24))
+#define AFE_SA_IS_SPL_IDX(X) ((((X) >= 3810) && ((X) < 3899)) ? 1 : 0)
+/* To Decode the Param ID */
+#define TAS_PARAM_IDX_GET_ID(X) (0xFFFF & (X))
+#define TAS_PARAM_IDX_GET_LEN(X) ((0x0FFF0000 & (X)) >> 16)
+#define TAS_PARAM_IDX_GET_CH(X) ((0xF0000000 & (X)) >> 24)
+
+typedef enum {
+	IV_SENSE_FORMAT_NO_VBAT = 0,
+	IV_SENSE_FORMAT_12_BIT_WITH_8BIT_VBAT = 1,
+	IV_SENSE_FORMAT_8_BIT_WITH_8BIT_VBAT = 2,
+} ti_smartamp_iv_vbat_format_t;
+
+/*
+ * List all the other profiles other than none and calibration.
+ */
+#define TAS_ALGO_PROFILE_LIST "MUSIC", "VOICE", "VOIP", "RINGTONE"
+
+struct afe_smartamp_get_set_params_t {
+	uint32_t payload[TAS_PAYLOAD_SIZE];
+} __packed;
+
+#if APR_VERSION_V2
+struct afe_smartamp_config_command {
+	struct apr_hdr hdr;
+	struct afe_port_cmd_set_param_v2 param;
+	struct afe_port_param_data_v2 pdata;
+	struct afe_smartamp_get_set_params_t prot_config;
+} __packed;
+
+struct afe_smartamp_get_calib {
+	struct apr_hdr hdr;
+	struct afe_port_cmd_get_param_v2 get_param;
+	struct afe_port_param_data_v2 pdata;
+	struct afe_smartamp_get_set_params_t res_cfg;
+} __packed;
+
+struct afe_smartamp_calib_get_resp {
+	uint32_t status;
+	struct afe_port_param_data_v2 pdata;
+	struct afe_smartamp_get_set_params_t res_cfg;
+} __packed;
+
+#elif APR_VERSION_V3
+
+//#define BUILD_STUB
+/* only for build test */
+#ifdef BUILD_STUB
+#define INSTANCE_ID_0 0
+#define AFE_PORT_CMDRSP_GET_PARAM_V3 0
+struct param_hdr_v3 {
+	int module_id;
+	int instance_id;
+	int param_id;
+	int param_size;
+};
+
+struct mem_mapping_hdr {};
+
+#endif
+
+struct afe_smartamp_get_calib {
+	struct apr_hdr hdr;
+	struct mem_mapping_hdr mem_hdr;
+	struct param_hdr_v3 pdata;
+	struct afe_smartamp_get_set_params_t res_cfg;
+} __packed;
+
+struct afe_smartamp_calib_get_resp {
+	uint32_t status;
+	struct param_hdr_v3 pdata;
+	struct afe_smartamp_get_set_params_t res_cfg;
+} __packed;
+#endif
+
+typedef struct {
+	int channel;
+} dc_detection_data_t;
+
+void register_tas256x_reset_func(void *fptr, dc_detection_data_t *data);
+int afe_tas_smartamp_get_calib_data(uint32_t module_id, uint32_t param_id,
+				    int32_t length, uint8_t *data);
+int afe_tas_smartamp_set_calib_data(uint32_t module_id, uint32_t param_id,
+				    int32_t length, uint8_t *data);
+
+int tas25xx_smartamp_algo_ctrl(u8 *user_data, uint32_t param_id,
+			       uint8_t get_set, uint32_t length,
+			       uint32_t module_id);
+
+void tas_smartamp_add_algo_controls(struct snd_soc_component *component);
+//void tas_smartamp_add_algo_controls_for_platform (struct snd_soc_platform *platform);
+void tas_smartamp_remove_algo_controls(struct snd_soc_component *component);
+bool tas25xx_set_iv_bit_fomat(int iv_data_with, int vbat, int update_now);
+#endif